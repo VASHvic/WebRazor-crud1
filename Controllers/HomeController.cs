ea6f5d04bbf483b469910fcefa3d9851a14d3da7
VASHvic <vforvanish@gmail.com>
2022-02-02 20:11:12 +0100

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using WebRazor.Models;
+
+namespace WebRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            MantenimientoArticulo ma = new MantenimientoArticulo();
+            return View(ma.Listar());
+        }
+    }
+}
---
+++ Models/MantenimientoArticulo.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebRazor.Models
+{
+    public class MantenimientoArticulo
+    {
+        private SqlConnection con;
+        private void Conectar()
+        {
+            //RouterConfig = new RouterCOnfig();
+            string constr = ConfigurationManager.ConnectionStrings["administracion"].ToString();
+            con = new SqlConnection(constr);
+        }
+        public int Alta(Articulo art)
+        {
+            Conectar();
+            SqlCommand comando = new SqlCommand("insert into articulos(codigo,descripcion,precio) values (@codigo,@descripcion,@precio)", con);
+            comando.Parameters.Add("@codigo", SqlDbType.Int);
+            comando.Parameters.Add("@descripcion", SqlDbType.VarChar);
+            comando.Parameters.Add("@precio", SqlDbType.Float);
+            comando.Parameters["@codigo"].Value = art.Codigo;
+            comando.Parameters["@descripcion"].Value = art.Descripcion;
+            comando.Parameters["@precio"].Value = art.Precio;
+            con.Open();
+            int i = comando.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+        public List<Articulo> Listar()
+        {
+            Conectar();
+            List<Articulo> articulos = new List<Articulo>();
+
+            SqlCommand com = new SqlCommand("select codigo,descripcion,precio from articulos", con);
+            con.Open();
+            SqlDataReader registros = com.ExecuteReader();
+            while (registros.Read())
+            {
+                Articulo art = new Articulo
+                {
+                    Codigo = int.Parse(registros["codigo"].ToString()),
+                    Descripcion = registros["descripcion"].ToString(),
+                    Precio = float.Parse(registros["precio"].ToString())
+                };
+                articulos.Add(art);
+            }
+            con.Close();
+            return articulos;
+        }
+    }
+}
