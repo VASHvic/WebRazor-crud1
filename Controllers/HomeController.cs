c131397060f77979716500772bd99725c1f336bd
VASHvic <vforvanish@gmail.com>
2022-02-03 19:19:43 +0100

    alta feta

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,5 +11,22 @@ namespace WebRazor.Controllers
             MantenimientoArticulo ma = new MantenimientoArticulo();
             return View(ma.Listar());
         }
+        public ActionResult Alta()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Alta(FormCollection collection)
+        {
+            Articulo art = new Articulo();
+            //art.Codigo = int.Parse(collection["codigo"]);
+            art.Descripcion = collection["descripcion"];
+            art.Precio = float.Parse(collection["precio"]);
+            MantenimientoArticulo man = new MantenimientoArticulo();
+            man.Alta(art);
+            return RedirectToAction("Index"); ;
+        }
+
+
     }
 }
--- Models/MantenimientoArticulo.cs
+++ Models/MantenimientoArticulo.cs
@@ -11,17 +11,17 @@ namespace WebRazor.Models
         private void Conectar()
         {
             //RouterConfig = new RouterCOnfig();
-            string constr = ConfigurationManager.ConnectionStrings["administracion"].ToString();
+            string constr = ConfigurationManager.ConnectionStrings["administracion"].ToString(); // llig de web.config
             con = new SqlConnection(constr);
         }
         public int Alta(Articulo art)
         {
             Conectar();
-            SqlCommand comando = new SqlCommand("insert into articulos(codigo,descripcion,precio) values (@codigo,@descripcion,@precio)", con);
-            comando.Parameters.Add("@codigo", SqlDbType.Int);
+            SqlCommand comando = new SqlCommand("insert into articulos(descripcion,precio) values (@descripcion,@precio)", con);
+            //comando.Parameters.Add("@codigo", SqlDbType.Int);
             comando.Parameters.Add("@descripcion", SqlDbType.VarChar);
             comando.Parameters.Add("@precio", SqlDbType.Float);
-            comando.Parameters["@codigo"].Value = art.Codigo;
+            //comando.Parameters["@codigo"].Value = art.Codigo;
             comando.Parameters["@descripcion"].Value = art.Descripcion;
             comando.Parameters["@precio"].Value = art.Precio;
             con.Open();
@@ -49,6 +49,17 @@ namespace WebRazor.Models
             }
             con.Close();
             return articulos;
+        }
+        public int Borrar(int codigo)
+        {
+            Conectar();
+            SqlCommand comando = new SqlCommand("delete from articulos where codigo=@codigo", con);
+            comando.Parameters.Add("@codigo", SqlDbType.Int);
+            comando.Parameters["@codigo"].Value = codigo;
+            con.Open();
+            int i = comando.ExecuteNonQuery();
+            con.Close();
+            return i;
         }
     }
 }
